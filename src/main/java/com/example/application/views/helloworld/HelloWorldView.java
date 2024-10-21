e3086970d521203af08c19bcfbe35f65cae2f7f3
Petri Heinonen <petri@vaadin.com>
2024-10-21 13:41:21 +0300

    Added logout button if auth context exists to HelloWorldView

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,14 +1,17 @@
 package com.example.application.views.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.spring.security.AuthenticationContext;
 
 import jakarta.annotation.security.RolesAllowed;
 
@@ -16,23 +19,27 @@ import jakarta.annotation.security.RolesAllowed;
 @Menu(icon = "line-awesome/svg/globe-solid.svg", order = 0)
 @Route(value = "hello")
 @RolesAllowed({"user", "admin"})
-public class HelloWorldView extends HorizontalLayout {
+public class HelloWorldView extends VerticalLayout {
 
     private TextField name;
     private Button sayHello;
 
-    public HelloWorldView() {
+    public HelloWorldView(@Autowired AuthenticationContext authenticationContext) {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
         sayHello.addClickShortcut(Key.ENTER);
-
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        HorizontalLayout hl = new HorizontalLayout(name, sayHello); 
+        hl.setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        add(hl);
 
-        add(name, sayHello);
+        //Only add the logut button if there is an auth context... 
+        if(authenticationContext != null){
+            add(new Button("Logout", e -> authenticationContext.logout()));
+        }
     }
 
 }
--- src/main/java/com/example/application/views/secured/SecuredView.java
+++ src/main/java/com/example/application/views/secured/SecuredView.java
@@ -1,14 +1,10 @@
 package com.example.application.views.secured;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.security.AuthenticationContext;
 
 import jakarta.annotation.security.RolesAllowed;
 
@@ -19,9 +15,8 @@ import jakarta.annotation.security.RolesAllowed;
 public class SecuredView extends VerticalLayout{
 
     
-    public SecuredView(@Autowired AuthenticationContext authenticationContext){
-        add(new Text("Secured view"));
-        add(new Button("Logout", e -> authenticationContext.logout()));
+    public SecuredView(){
+        add(new Text("Secured view, admins eyes only..."));
     }
 
 
